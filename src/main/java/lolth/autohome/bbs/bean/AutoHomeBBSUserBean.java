579f350644b3a70bdfce12de7ec81c68da524ee3
shilei <shi.lei@pagechoice.com>
2015-03-13 09:50:02 +0800

    增加汽车之家用户爬取，百度正文提取逻辑修改。

---
+++ src/main/java/lolth/autohome/bbs/bean/AutoHomeBBSUserBean.java
@@ -0,0 +1,63 @@
+package lolth.autohome.bbs.bean;
+
+import java.sql.SQLException;
+
+import lakenono.db.BaseBean;
+import lakenono.db.annotation.DBTable;
+
+@DBTable(name = "data_autohome_bbs_user")
+public class AutoHomeBBSUserBean extends BaseBean {
+
+	// 作者地址
+	private String authorUrl;
+	// 区域
+	private String from;
+
+	private String car;
+	// 关注
+	private String concern;
+
+	public static void main(String[] args) throws SQLException {
+		new AutoHomeBBSUserBean().buildTable();
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getCar() {
+		return car;
+	}
+
+	public void setCar(String car) {
+		this.car = car;
+	}
+
+	public String getConcern() {
+		return concern;
+	}
+
+	public void setConcern(String concern) {
+		this.concern = concern;
+	}
+
+	public String getAuthorUrl() {
+		return authorUrl;
+	}
+
+	public void setAuthorUrl(String authorUrl) {
+		this.authorUrl = authorUrl;
+	}
+
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append(from).append('|');
+		str.append(car).append('|');
+		str.append(concern);
+		return str.toString();
+	}
+}
