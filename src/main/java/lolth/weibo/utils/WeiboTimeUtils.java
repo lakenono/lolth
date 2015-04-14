1d1fba0e77eca888c1e863820d986763076c5dda
shilei9213 <shilei9213@gmail.com>
2015-04-14 15:33:44 +0800

    微博时间处理工具，统一为yyyy-MM-dd HH:mm:ss

--- src/main/java/lolth/weibo/utils/WeiboTimeUtils.java
+++ src/main/java/lolth/weibo/utils/WeiboTimeUtils.java
@@ -6,16 +6,16 @@ import java.time.format.DateTimeFormatter;
 import org.apache.commons.lang3.StringUtils;
 
 public class WeiboTimeUtils {
-	
+
 	/**
-	 * 日期形式：48分钟前，03月29日 20:40，今天 08:32
-	 * 转换格式：2014-09-29 02:18:02 
+	 * 日期形式：48分钟前，03月29日 20:40，今天 08:32 转换格式：2014-09-29 02:18:02
+	 * 
 	 * @param postTime
 	 * @param now
 	 * @return
 	 */
-	public static  String getNormalTime(String postTime, LocalDateTime now) {
-		//48分钟前
+	public static String getNormalTime(String postTime, LocalDateTime now) {
+		// 48分钟前
 		if (StringUtils.contains(postTime, "分钟前")) {
 			postTime = StringUtils.substringBefore(postTime, "分钟前");
 			postTime = StringUtils.trim(postTime);
@@ -24,38 +24,41 @@ public class WeiboTimeUtils {
 			}
 
 		}
-		
-		//今天 08:32
+
+		// 今天 08:32
 		if (StringUtils.contains(postTime, "今天")) {
 			String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-			return StringUtils.replaceOnce(postTime, "今天", date)+":00";
-			
+			postTime = StringUtils.replaceOnce(postTime, "今天", date);
+			postTime = StringUtils.trim(postTime);
+			return postTime + ":00";
+
 		}
-		
-		//03月29日 20:40
-		if(StringUtils.contains(postTime, "月")){
-			postTime = StringUtils.replaceChars(postTime, '月','-');
+
+		// 03月29日 20:40
+		if (StringUtils.contains(postTime, "月")) {
+			postTime = StringUtils.replaceChars(postTime, '月', '-');
+			postTime = StringUtils.trim(postTime);
 			postTime = StringUtils.remove(postTime, '日');
-			return now.getYear()+"-"+postTime+":00";
+			return now.getYear() + "-" + postTime + ":00";
 		}
-		
+
 		return postTime;
 	}
-	
-	public static boolean isBefore(String time,String timeRange){
+
+	public static boolean isBefore(String time, String timeRange) {
 		LocalDateTime timeObj = LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		LocalDateTime timeRangeObj = LocalDateTime.parse(timeRange, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		return timeObj.isBefore(timeRangeObj);
 	}
-	
+
 	public static void main(String[] args) {
-		String[] times = {"48分钟前","03月29日 20:40","2014-09-29 02:18:02","今天 08:32"};
-		for(String t : times){
-			String normalTime = getNormalTime(t,LocalDateTime.now());
+		String[] times = { "48分钟前", "03月29日 20:40 ", "2014-09-29 02:18:02", "今天 08:32 " };
+		for (String t : times) {
+			String normalTime = getNormalTime(t, LocalDateTime.now());
 			System.out.println(normalTime);
-			System.out.println(isBefore(normalTime,"2014-10-01 00:00:00"));
+			System.out.println(isBefore(normalTime, "2014-10-01 00:00:00"));
 			System.out.println("===========");
 		}
-		
+
 	}
 }
