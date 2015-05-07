0be5e2318522f7ac18b240e5b3cd9814d1bc62e7
shilei9213 <shilei9213@gmail.com>
2015-05-07 11:11:10 +0800

    修改为登陆认证的fetcher

--- src/main/java/lolth/baidu/post/task/BaiduPostDetailFetch.java
+++ src/main/java/lolth/baidu/post/task/BaiduPostDetailFetch.java
@@ -1,7 +1,10 @@
 package lolth.baidu.post.task;
 
+import java.io.IOException;
+
 import lakenono.task.FetchTask;
 import lakenono.task.PageParseFetchTaskHandler;
+import lolth.baidu.fetch.BaiduFetcher;
 import lolth.baidu.post.bean.BaiduPostBean;
 import lolth.baidu.post.bean.BaiduPostUserBean;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +20,12 @@ import com.alibaba.fastjson.JSONObject;
 public class BaiduPostDetailFetch extends PageParseFetchTaskHandler {
 
 	public static void main(String[] args) throws Exception {
-		 //找到生产者提交的队列
-		 String taskQueueName = BaiduPostListFetch.BAIDU_POST_DETAIL;
-		 //执行分布式抓取
-		 BaiduPostDetailFetch fetch = new BaiduPostDetailFetch(taskQueueName);
-		 fetch.setSleep(3000);
-		 fetch.run();
+		// 找到生产者提交的队列
+		String taskQueueName = BaiduPostListFetch.BAIDU_POST_DETAIL;
+		// 执行分布式抓取
+		BaiduPostDetailFetch fetch = new BaiduPostDetailFetch(taskQueueName);
+		fetch.setSleep(3000);
+		fetch.run();
 	}
 
 	public BaiduPostDetailFetch(String taskQueueName) {
@@ -32,70 +35,68 @@ public class BaiduPostDetailFetch extends PageParseFetchTaskHandler {
 	@Override
 	protected void parsePage(Document doc, FetchTask task) {
 		try {
-			BaiduPostBean postBean = getBaiduPostBean(doc,task);
-			BaiduPostUserBean userBean = getBaiduUserBean(doc,task,postBean);
-			
+			BaiduPostBean postBean = getBaiduPostBean(doc, task);
+			BaiduPostUserBean userBean = getBaiduUserBean(doc, task, postBean);
+
 			postBean.persistOnNotExist();
-			log.debug("Save BaiduPostUserBean {}",postBean);
-			
-			if(userBean!=null){
+			log.debug("Save BaiduPostUserBean {}", postBean);
+
+			if (userBean != null) {
 				userBean.persistOnNotExist();
-				
-				log.debug("Save BaiduPostUserBean {}",userBean);
+
+				log.debug("Save BaiduPostUserBean {}", userBean);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private BaiduPostUserBean getBaiduUserBean(Document doc, FetchTask task,BaiduPostBean postBean) {
-		
+
+	private BaiduPostUserBean getBaiduUserBean(Document doc, FetchTask task, BaiduPostBean postBean) {
+
 		BaiduPostUserBean userBean = null;
-		
+
 		Elements userDiv = doc.select("div.l_post.l_post_bright.noborder");
-		
-		if(userDiv.size()>0){
-			
+
+		if (userDiv.size() > 0) {
+
 			userBean = new BaiduPostUserBean();
 			String postInfo = userDiv.first().attr("data-field");
-			
+
 			JSONObject json = JSON.parseObject(postInfo);
-			
-			
+
 			JSONObject user = json.getJSONObject("author");
 			userBean.setId(user.getString("user_id"));
-			userBean.setUrl("http://tieba.baidu.com/home/main?un="+user.getString("name_u"));
-			
+			userBean.setUrl("http://tieba.baidu.com/home/main?un=" + user.getString("name_u"));
+
 			String sex = user.getString("user_sex");
-			if("0".equals(sex)||"1".equals(sex)){
+			if ("0".equals(sex) || "1".equals(sex)) {
 				userBean.setSex("男");
-			}else if("2".equals(sex)){
+			} else if ("2".equals(sex)) {
 				userBean.setSex("女");
 			}
-			
-			
+
 			JSONObject content = json.getJSONObject("content");
 			String postTime = content.getString("date");
-			
+
 			postBean.setPostTime(postTime);
 			postBean.setUserId(userBean.getId());
 		}
 		return userBean;
 	}
 
-	private BaiduPostBean getBaiduPostBean(Document doc, FetchTask task){
-		
-		//------------------------帖子--------------------------------------
+	private BaiduPostBean getBaiduPostBean(Document doc, FetchTask task) {
+
+		// ------------------------帖子--------------------------------------
 		BaiduPostBean post = new BaiduPostBean();
 
 		post.setId(StringUtils.substringAfterLast(task.getUrl(), "/"));
 		post.setUrl(task.getUrl());
 		post.setKeyword(task.getName());
-		
-		//板块
+
+		// 板块
 		Elements forum = doc.select("a.plat_title_h3");
-		if(forum.size() > 0){
+		if (forum.size() > 0) {
 			post.setForum(forum.first().text());
 		}
 
@@ -128,16 +129,23 @@ public class BaiduPostDetailFetch extends PageParseFetchTaskHandler {
 					post.setContent(content.first().text());
 				}
 			}
-			
-			//postTime
+
+			// postTime
 			Elements postTimeUl = leftSection.first().select("ul.p_tail");
-			if(postTimeUl.size()>0){
+			if (postTimeUl.size() > 0) {
 				String postTime = postTimeUl.first().text();
 				postTime = StringUtils.substringAfter(postTime, "1楼");
 				post.setPostTime(postTime);
 			}
 		}
 		return post;
+	}
+
+	@Override
+	protected void handleTask(FetchTask task) throws IOException, InterruptedException, Exception {
+		Document doc = BaiduFetcher.fetcher.document(task.getUrl());
+		parsePage(doc, task);
+		Thread.sleep(sleep);
 	}
 
 }
--- src/test/java/lolth/baidu/zhidao/BaiduZhidaoDetailFetchTest.java
+++ src/test/java/lolth/baidu/zhidao/BaiduZhidaoDetailFetchTest.java
@@ -1,7 +1,7 @@
 package lolth.baidu.zhidao;
 
-import lakenono.core.GlobalComponents;
 import lakenono.task.FetchTask;
+import lolth.baidu.fetch.BaiduFetcher;
 
 import org.jsoup.nodes.Document;
 import org.junit.BeforeClass;
@@ -18,7 +18,7 @@ public class BaiduZhidaoDetailFetchTest {
 	@Test
 	public void testZhidao() throws Exception {
 		String url = "http://zhidao.baidu.com/question/1174802513797007179.html?fr=iks&word=%BB%DD%CA%CF%C6%F4%B8%B3+%B8%DB%B0%E6+%C4%DA%B5%D8&ie=gbk";
-		Document doc = GlobalComponents.fetcher.document(url);
+		Document doc = BaiduFetcher.fetcher.document(url);
 
 		fetch.parsePage(doc, getTask(url));
 	}
@@ -26,7 +26,7 @@ public class BaiduZhidaoDetailFetchTest {
 	@Test
 	public void testZuoye() throws Exception {
 		String url = "http://zuoye.baidu.com/question/a0a50de360400197bfbcc988881379b6.html?fr=iks&word=%BB%DD%CA%CF&ie=gbk";
-		Document doc = GlobalComponents.fetcher.document(url);
+		Document doc = BaiduFetcher.fetcher.document(url);
 
 		fetch.parsePage(doc, getTask(url));
 	}
