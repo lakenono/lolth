17f91864d1aad68bd36f8ca82387a22645eeb2af
xierenjing <gengbushuang@gmail.com>
2015-05-20 14:38:58 +0800

    增加微博的提取

--- src/main/java/lolth/weibo/utils/WeiboContentSpliter.java
+++ src/main/java/lolth/weibo/utils/WeiboContentSpliter.java
@@ -62,6 +62,61 @@ public class WeiboContentSpliter {
 		bean.setFeelings(feelings.toString());
 	}
 
+	public static String[] spliteContent(String text) {
+		if (StringUtils.isBlank(text)) {
+			return null;
+		}
+		String[] strs = new String[4];
+		StringBuffer at = new StringBuffer();
+		StringBuffer topic = new StringBuffer();
+		StringBuffer mainText = new StringBuffer();
+		StringBuffer feelings = new StringBuffer();
+
+		boolean needMainText = true;
+		// 评论
+		char[] chars = text.toCharArray();
+		int charIdx = 0;
+		while (charIdx < chars.length) {
+			switch (chars[charIdx]) {
+			case '#':
+				charIdx = segmentText(topic, chars, charIdx, '#');
+				// 添加结束索引
+				topic.append('#');
+				// 移动游标，表示结束字符处理完成
+				charIdx++;
+				break;
+			case '@':
+				charIdx = segmentText(at, chars, charIdx, '#', ' ', ':', ';');
+				break;
+			case '[':
+				charIdx = segmentText(feelings, chars, charIdx, ']');
+				feelings.append(']');
+				charIdx++;
+				break;
+			case '/':
+				if (needMainText) {
+					if (charIdx + 2 < chars.length && chars[charIdx + 1] == '/' && chars[charIdx + 2] == '@') {
+						needMainText = false;
+					}
+				}
+			default:
+				if (needMainText) {
+					mainText.append(chars[charIdx]);
+				}
+
+			}
+			charIdx++;
+		}
+
+		if (StringUtils.contains(text, "//@")) {
+			strs[0] = "//@" + StringUtils.substringAfter(text, "//@");
+		}
+		strs[1] = at.toString();
+		strs[2] = mainText.toString();
+		strs[3] = topic.toString();
+		return strs;
+	}
+
 	// 返回结束标签索引
 	private static int segmentText(StringBuffer buffer, char[] chars, int curCharIdx, char... endChars) {
 		// 添加开始字符
