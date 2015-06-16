92b72af5a0268655d246b8c9c011bc14de3f32e7
xierenjing <gengbushuang@gmail.com>
2015-06-16 15:18:42 +0800

    微博移动2的需求

--- src/main/java/lolth/weibo/auth/WeiboAuthManagerImpl.java
+++ src/main/java/lolth/weibo/auth/WeiboAuthManagerImpl.java
@@ -15,10 +15,10 @@ public class WeiboAuthManagerImpl implements WeiboAuthManager {
 	public WeiboAuthManagerImpl() {
 
 		List<String> cookieList = new ArrayList<>();
-		cookieList.add("_T_WM=4c8673afeeaf7be5e7dd78359b66a34b; SUB=_2A254c9tfDeTxGedH7VQS9irNwjmIHXVbn-UXrDV6PUJbrdANLVnWkW1BbC5-YMwGqGyxytEHaNTnjyUY8Q..");
-		cookieList.add("_T_WM=ac0612a664838b72b7d8662f0df280c1; SUB=_2A254c90BDeTxGeRP6VQV8CvLzD2IHXVbn-NJrDV6PUJbrdAKLVDtkW2eswvsU-yAXQyByi6neN9OZ0yNVQ..; gsid_CTandWM=4uPte42b1SJdUV3UAjo528VaRcr");
-		cookieList.add("_T_WM=9fd33549a75d82ecd51a238f80937e18; SUB=_2A254c90ADeTxGeNI61cW8S7FyTmIHXVbn-NIrDV6PUJbrdAKLW_ykW2IZiTXzAsI3ego2c6Fs41DeHOuVA..; gsid_CTandWM=4u4Ndbd01GuCtKJJxIUsSnwiGeV");
-		
+		cookieList.add("_T_WM=ac0612a664838b72b7d8662f0df280c1; SUB=_2A254ews9DeSRGeNL7FUR-C7NyjqIHXVbh5V1rDV6PUJbrdAKLWKjkW2Ff_rOGAIhis2WmvGEDWxEFsFIfg..; gsid_CTandWM=4u3ee42b175MiIfztGzkZnoQU1S; M_WEIBOCN_PARAMS=from%3Dhome");
+		cookieList.add("_T_WM=d0d1e55c38772164698110133145a81c; SUB=_2A254ewzqDeSRGeNL7FQS9inIyj-IHXVbh5SirDV6PUJbrdAKLRbbkW2KktXXSiCCEPger3MAtLGkuOO2sA..; gsid_CTandWM=4ueEe42b1ArRJkT34m8yVnoFp6F");
+		cookieList.add("_T_WM=437f0da84da2c5d37944acade4a3f4c1; SUB=_2A254e7f_DeSRGeNL7FQW8CnIyjqIHXVbh9m3rDV6PUJbrdANLXPBkW1_l2tTIH07_6CFc5FaFyzWtB914w..; gsid_CTandWM=4uEpe42b1EBvPOzjXYqnInoKT6I");
+		cookieList.add("_T_WM=217fecb7b067dd37173163c7c57432e0; SUB=_2A254e7tWDeTxGeRP6VQV8CvLzD2IHXVbh8UerDV6PUJbrdAKLWfekW1fkfvSFyZV9sMTeRi9a5XmTWYfQQ..; gsid_CTandWM=4u8te42b1P83L7DWZS8Mn8VaRcr");
 		for (String cookieStr : cookieList) {
 			Map<String, String> cookieMap = parseCookieStr(cookieStr);
 			if (!cookieMap.isEmpty()) {
