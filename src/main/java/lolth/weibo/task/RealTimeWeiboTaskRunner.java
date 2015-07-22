a8dd1eebd1e5fd0b350e43a17b09a32d6d0f3c5b
shilei9213 <shilei9213@126.com>
2015-07-22 16:02:48 +0800

    传递关键字参数

--- src/main/java/lolth/weibo/task/RealTimeWeiboTaskRunner.java
+++ src/main/java/lolth/weibo/task/RealTimeWeiboTaskRunner.java
@@ -7,9 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 public class RealTimeWeiboTaskRunner {
 	public static void main(String[] args) throws Exception {
 		long sleep = 10 * 60 * 1000;
-		String keyword = "oppo";
+		if (args.length == 0) {
+			System.out.println("Useage : RealTimeWeiboTaskRunner keyword !");
+			return;
+		}
+		String keyword = args[0];
 		while (true) {
 			try {
+				log.info("start {} fetch ...",keyword);
 				new WeiboSearchFetch(keyword).run();
 				log.info("Wait for next batch fetch ... ");
 				Thread.sleep(sleep);
