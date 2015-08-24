81bc617305c41783e9d8c9fd5dc7d7ba3ec5346c
shilei9213 <shilei9213@126.com>
2015-08-24 14:58:01 +0800

    dmp 电商 bean

---
+++ src/main/java/dmp/ec/ECBean.java
@@ -0,0 +1,45 @@
+package dmp.ec;
+
+import lakenono.db.DBBean;
+import lakenono.db.DMPBean;
+import lakenono.db.annotation.DBConstraintPK;
+import lakenono.db.annotation.DBTable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@DBTable(name = "data_dmp_ec")
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class ECBean extends DBBean {
+	// 商品ID
+	@DBConstraintPK
+	private String id;
+	// 商品url
+	private String url;
+	// 商品名称
+	private String title;
+	// 商品类别
+	private String category;
+	// 提取关键字
+	@DBConstraintPK
+	private String keyword;
+
+	public ECBean(String tableKey) {
+		super(tableKey);
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		// 建表时制定网站来源
+		DBBean.createTable(DMPBean.class, "taobao");
+
+		// 插入时指定网站来源
+		ECBean taobaoBean = new ECBean("taobao");
+		taobaoBean.setId("1");
+		taobaoBean.setUrl("舍HI");
+		taobaoBean.setTitle("t");
+		taobaoBean.setCategory("123>123>1");
+
+		taobaoBean.saveOnNotExist();
+	}
+}
