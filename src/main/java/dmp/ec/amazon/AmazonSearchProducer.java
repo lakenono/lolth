1ab30385d421d38a92e19f93fbc6dcee6829ca35
shilei9213 <shilei9213@126.com>
2015-08-27 14:47:21 +0800

    亚马逊dmp 程序修调整继承规范

--- src/main/java/dmp/ec/amazon/AmazonSearchProducer.java
+++ src/main/java/dmp/ec/amazon/AmazonSearchProducer.java
@@ -3,7 +3,6 @@ package dmp.ec.amazon;
 import java.text.MessageFormat;
 
 import lakenono.base.Producer;
-import lakenono.base.Queue;
 import lakenono.base.Task;
 import lakenono.core.GlobalComponents;
 import lakenono.util.UrlUtils;
@@ -15,15 +14,12 @@ public class AmazonSearchProducer extends Producer {
 
 	// 模板url
 	private final String templetUrl = "http://www.amazon.cn/s/page={0}&keywords={1}";
-	// 项目名称
-	private String projectName;
 	// 关键字
 	private String keyword;
 	// 队列名称
 	public static final String QUEUENAME = "amazon_search_list";
 
 	public AmazonSearchProducer(String projectName, String keyword) {
-		// this.projectName = projectName;
 		super(projectName);
 		this.keyword = keyword;
 	}
@@ -37,24 +33,10 @@ public class AmazonSearchProducer extends Producer {
 		}
 	}
 
-	public void run() throws Exception {
-		int maxpage = this.parse();
-
-		for (int i = 1; i <= maxpage; i++) {
-			String url = MessageFormat.format(templetUrl, i, UrlUtils.encode(keyword));
-			Task task1 = buildTask(url);
-			Queue.push(task1);
-		}
-
-	}
-
+	@Override
 	protected Task buildTask(String url) {
-		Task task = new Task();
+		Task task = super.buildTask(url);
 		task.setProjectName(this.projectName);
-		task.setQueueName(QUEUENAME);
-		task.setUrl(url);
-		task.setExtra(this.keyword);
-
 		return task;
 	}
 
