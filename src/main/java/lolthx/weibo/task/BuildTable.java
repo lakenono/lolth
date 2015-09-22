85f7b01604ea9c34c09a9ebfc1d41746bd0f7622
yanghp <yanghp@bogon>
2015-09-22 16:33:50 +0800

    添加微博userbean表

--- src/main/java/lolthx/weibo/task/BuildTable.java
+++ src/main/java/lolthx/weibo/task/BuildTable.java
@@ -4,12 +4,14 @@ import java.sql.SQLException;
 
 import lakenono.db.DBBean;
 import lolthx.weibo.bean.WeiboBean;
+import lolthx.weibo.bean.WeiboUserBean;
 
 public class BuildTable {
 	
 	public static void main(String[] args) throws SQLException {
 		String projectName = "ceshi";
 		DBBean.createTable(WeiboBean.class, projectName);
+		DBBean.createTable(WeiboUserBean.class, projectName);
 	}
 
 }
