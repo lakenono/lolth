3d099c813f152b7c4d2063dda4249f09ba7468f7
xierenjing <gengbushuang@gmail.com>
2015-11-16 11:32:17 +0800

    提交推送任务华扬联众演讲数据

--- src/main/java/lolthx/hers/task/HersSearchTask.java
+++ src/main/java/lolthx/hers/task/HersSearchTask.java
@@ -67,4 +67,16 @@ public class HersSearchTask{
 	private String buildUrl() throws Exception {
 		return MessageFormat.format(base_url, URLDecoder.decode(keyword, "utf-8"));
 	}
+	
+	public static void main(String[] args) {
+		String[] keywords = {"LaMer","海蓝之谜","HR","赫莲娜 ","CHANEL","香奈儿","Dior","迪奥 ","EsteeLauder","雅诗兰黛","SK-II ","LANCOME","兰蔻 ","ElizabethArden","伊丽莎白雅顿","Clarins","娇韵诗","Sisley","希思黎","Guerlain","娇兰","Clinique","倩碧","Maybelline","美宝莲","Avene","雅漾","Mentholatum","曼秀雷敦","SHISEIDO","资生堂 ","Biotherm","碧欧泉","Olay","玉兰油","L'OREAL","巴黎欧莱雅 ","AUPRES","欧珀莱","VICHY","薇姿","NIVEA","妮维雅","kiehl's","契尔氏","Garnier","卡尼尔","DHC","蝶翠诗","Za","姬芮","ARTISTRY","雅姿","Charmzone","婵真","Mamonde","梦妆 ","hera","赫拉 ","Innisfree","悦诗风吟 ","SKINFOOD","THE FACE SHOP","谜尚","MISSHA","it`s skin","伊思","IOPE","LANEIGE","兰芝 ","Sulwhasoo","雪花秀","whoo后","ohui","欧蕙 ","ETUDE HOUSE","爱丽小屋","相宜本草","LouisVuitton","路易威登","Gucci","古驰","CHANL","香奈儿","小香","PRADA","普拉达","HERMES","爱马仕","Dior","迪奥","bottegaveneta","葆蝶家","Burberry","博柏利","巴宝莉","chloe","蔻依","克洛伊","AlexanderMqueen","亚历山大·麦昆","marcjacobs","马克·雅可布","MansurGavriel","曼苏丽尔","versace","范思哲","CELINE","思琳","赛琳","FENDI","芬迪","MiuMiu","缪缪","givenchy","纪梵希","marcbymarcjacobs","Coach","蔻驰","寇驰","MichaelKors","迈克.科尔斯","Katespade","凯特·丝蓓","ToryBurch","汤丽柏琦","MCM","Furla","芙拉","GerardDarel","杰哈·达黑勒"};
+		String projectName = "A20151113";
+		for(String keyword:keywords){
+			try {
+				new HersSearchTask(projectName,keyword).run();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
