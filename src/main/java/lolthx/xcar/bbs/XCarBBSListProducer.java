6d6f017713798aae3dd8041b42ac2a11db9fd4a0
wuhao666 <sky-boy-sunny@126.com>
2015-12-16 10:54:42 +0800

    爱开汽车 page页修改

--- src/main/java/lolthx/xcar/bbs/XCarBBSListProducer.java
+++ src/main/java/lolthx/xcar/bbs/XCarBBSListProducer.java
@@ -11,21 +11,22 @@ import lakenono.base.Producer;
 import lakenono.base.Task;
 import lakenono.core.GlobalComponents;
 
-public class XCarBBSListProducer extends Producer{
-	
+public class XCarBBSListProducer extends Producer {
+
 	private static final String XCAR_BBS_LIST_URL = "http://www.xcar.com.cn/bbs/forumdisplay.php?fid={0}&orderby=dateline&page={1}";
-			//"http://www.xcar.com.cn/bbs/forumdisplay.php?fid={0}&page={1}"
+	// "http://www.xcar.com.cn/bbs/forumdisplay.php?fid={0}&page={1}"
 
 	private String id;
-	
+
 	private String keyword;
-	
-	private int page;
-	
-	public XCarBBSListProducer(String projectName,String id,String keyword) {
+
+	private int pageInt;
+
+	public XCarBBSListProducer(String projectName, String id, String keyword, int pageInt) {
 		super(projectName);
 		this.id = id;
 		this.keyword = keyword;
+		this.pageInt = pageInt;
 	}
 
 	@Override
@@ -35,24 +36,27 @@ public class XCarBBSListProducer extends Producer{
 
 	@Override
 	protected int parse() throws Exception {
-			Document doc = GlobalComponents.fetcher.document(buildUrl(1));
-			Elements pages = doc.select("div.fn_0209 a");
-			if (pages.size() == 1) {
-				return 1;
-			}
-			if (pages.size() > 2) {
-				int maxPageIdx = pages.size() - 2;
-				String pageStr = pages.get(maxPageIdx).text();
-				return Integer.parseInt(pageStr);
-			}
-			return 0;
+		if (pageInt != 0) {
+			return pageInt;
+		}
+		Document doc = GlobalComponents.fetcher.document(buildUrl(1));
+		Elements pages = doc.select("div.fn_0209 a");
+		if (pages.size() == 1) {
+			return 1;
+		}
+		if (pages.size() > 2) {
+			int maxPageIdx = pages.size() - 2;
+			String pageStr = pages.get(maxPageIdx).text();
+			return Integer.parseInt(pageStr);
+		}
+		return 0;
 	}
 
 	@Override
 	protected String buildUrl(int pageNum) throws Exception {
 		return MessageFormat.format(XCAR_BBS_LIST_URL, id, String.valueOf(pageNum));
 	}
-	
+
 	@Override
 	protected Task buildTask(String url) {
 		Task buildTask = super.buildTask(url);
@@ -65,17 +69,16 @@ public class XCarBBSListProducer extends Producer{
 		}
 		return buildTask;
 	}
-	
-	public static void main(String args[]) throws Exception{
-		String projectName = "XCar_20150807";
 
-		String[] ids = {"959","847"};
-		String[] keywords = {"逸动EV","北汽EV160"};
-		for(int i = 0 ; i < ids.length ; i++){
-			new XCarBBSListProducer(projectName, ids[i], keywords[i]).run();
+	public static void main(String args[]) throws Exception {
+		String projectName = "埃尼比稿-爱卡汽车-20151216";
+		String[] ids = { "118" };
+		String[] keywords = { "机油论坛" };
+		int[] pages = { 120 };
+		for (int i = 0; i < ids.length; i++) {
+			new XCarBBSListProducer(projectName, ids[i], keywords[i], pages[i]).run();
 		}
-		
-		
+
 	}
 
 }
